150c783e6a2d7ba581345ebc27bd4872ca2a13a9
Nutt Chairatana <nuttchai@outlook.com>
2022-05-03 05:23:14 +0700

    React Lifecycle, Implement componentDidMount

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class Movies extends Component {
+  state = { movies: [] };
+
+  // React Lifecycle Ref: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+  componentDidMount() {
+    this.setState({
+      movies: [
+        { id: 1, title: "The Shawshank Redemption", runtime: 142 },
+        { id: 2, title: "The Godfather", runtime: 175 },
+        { id: 3, title: "The Dark Knight", runtime: 153 },
+      ],
+    });
+  }
+
   render() {
-    return <h2>Choose a movie</h2>;
+    return (
+      // Fragment wraps everything in a single element
+      <Fragment>
+        <h2>Choose a movie</h2>
+
+        <ul>
+          {this.state.movies.map((m) => (
+            <li key={m.id}>{m.title}</li>
+          ))}
+        </ul>
+      </Fragment>
+    );
   }
 }

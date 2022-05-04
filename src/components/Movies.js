29f36d50033715c0f289ea74cef5d555a3c0fb83
Nutt Chairatana <nuttchai@outlook.com>
2022-05-04 18:39:45 +0700

    Error Handling

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,24 +2,42 @@ import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
-  state = { movies: [], isLoaded: false };
+  state = { movies: [], isLoaded: false, error: null };
 
   // React Lifecycle Ref: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
   componentDidMount() {
     fetch("http://localhost:4000/v1/movies")
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status !== "200") {
+          let err = Error;
+          err.message = `Invalid response code: ${response.status}`;
+          this.setState({ error: err });
+        }
+        return response.json();
+      })
       .then((json) => {
-        this.setState({
-          isLoaded: true,
-          movies: json.movies,
-        });
+        this.setState(
+          {
+            isLoaded: true,
+            movies: json.movies,
+          },
+          // NOTE: check API error
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
       });
   }
 
   render() {
-    const { isLoaded, movies } = this.state;
+    const { isLoaded, movies, error } = this.state;
 
-    if (!isLoaded) {
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
       return <div>Loading...</div>;
     }
 

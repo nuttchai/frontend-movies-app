de0d29ff5a9a3c413de4e9a99b8f848a3ecacb3f
Nutt Chairatana <nuttchai@outlook.com>
2022-05-04 18:59:05 +0700

    Displaying a movie detail

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,66 @@
 import React, { Component, Fragment } from "react";
 
 export default class Movie extends Component {
-  state = { movie: {} };
+  state = { movie: {}, isLoaded: false, error: null };
 
   componentDidMount() {
-    this.setState({
-      movie: {
-        id: this.props.match.params.id,
-        title: "Some movie",
-        runtime: 150,
-      },
-    });
+    fetch(`http://localhost:4000/v1/movie/${this.props.match.params.id}`)
+      .then((response) => {
+        if (response.status !== "200") {
+          let err = Error;
+          err.message = `Invalid response code: ${response.status}`;
+          this.setState({ error: err });
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.setState(
+          {
+            isLoaded: true,
+            movie: json.movie,
+          },
+          // NOTE: check API error
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+      });
   }
 
   render() {
+    const { isLoaded, movie, error } = this.state;
+    if (movie.genres) {
+      movie.genres = Object.values(movie.genres);
+    } else {
+      movie.genres = [];
+    }
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <Fragment>
         <h2>
-          Movie: {this.state.movie.title} {this.state.movie.id}
+          Movie: {movie.title} ({movie.year})
         </h2>
-
+        <div className="float-start">
+          <small>Rating: {movie.mpaa_rating}</small>
+        </div>
+        <div className="float-end">
+          {movie.genres.map((m, index) => (
+            <span className="badge bg-secondary me-1" key={index}>
+              {m}
+            </span>
+          ))}
+        </div>
+        <div className="clearfix"></div>
+        <hr />
         <table className="table table-compact table-striped">
           <thead></thead>
           <tbody>
@@ -27,13 +68,19 @@ export default class Movie extends Component {
               <td>
                 <strong>Title:</strong>
               </td>
-              <td>{this.state.movie.title}</td>
+              <td>{movie.title}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Description:</strong>
+              </td>
+              <td>{movie.description}</td>
             </tr>
             <tr>
               <td>
                 <strong>Run time:</strong>
               </td>
-              <td>{this.state.movie.runtime} minutes</td>
+              <td>{movie.runtime} minutes</td>
             </tr>
           </tbody>
         </table>

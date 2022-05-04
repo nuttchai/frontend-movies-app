932c7a1cce8d7af4b7f896c0466cb0142207d74e
Nutt Chairatana <nuttchai@outlook.com>
2022-05-04 18:34:28 +0700

    Getting the list of movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,27 +2,34 @@ import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
-  state = { movies: [] };
+  state = { movies: [], isLoaded: false };
 
   // React Lifecycle Ref: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
   componentDidMount() {
-    this.setState({
-      movies: [
-        { id: 1, title: "The Shawshank Redemption", runtime: 142 },
-        { id: 2, title: "The Godfather", runtime: 175 },
-        { id: 3, title: "The Dark Knight", runtime: 153 },
-      ],
-    });
+    fetch("http://localhost:4000/v1/movies")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          movies: json.movies,
+        });
+      });
   }
 
   render() {
+    const { isLoaded, movies } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
     return (
       // Fragment wraps everything in a single element
       <Fragment>
         <h2>Choose a movie</h2>
 
         <ul>
-          {this.state.movies.map((m) => (
+          {movies.map((m) => (
             <li key={m.id}>
               <Link to={`/movies/${m.id}`}> {m.title} </Link>
             </li>

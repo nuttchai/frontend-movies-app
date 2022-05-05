668bbf49557cfb4c27d187c95832ce81ebe6a877
nuttchai <nuttchai@outlook.com>
2022-05-05 15:25:13 +0700

    Providing feedback with a reusable alert

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -3,7 +3,7 @@ import "./EditMovie.css";
 import Input from "./form-components/Input";
 import Select from "./form-components/Select";
 import TextArea from "./form-components/TextArea";
-
+import Alert from "./ui-components/Alert";
 export default class EditMovie extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +27,10 @@ export default class EditMovie extends Component {
       isLoaded: false,
       error: null,
       errors: [],
+      alert: {
+        type: "d-none",
+        message: "",
+      },
     };
 
     this.handleChange = this.handleChange.bind(this); // to handle when input is changed
@@ -59,7 +63,22 @@ export default class EditMovie extends Component {
 
     fetch("http://localhost:4000/v1/admin/editmovie", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        if (data.error) {
+          this.setState({
+            alert: {
+              type: "alert-danger",
+              message: data.error.message,
+            },
+          });
+        }
+        this.setState({
+          alert: {
+            type: "alert-success",
+            message: "Changes saved successfully!",
+          },
+        });
+      });
   };
 
   handleChange = (event) => {
@@ -131,6 +150,10 @@ export default class EditMovie extends Component {
     return (
       <Fragment>
         <h2>Add/Edit Movie</h2>
+        <Alert
+          type={this.state.alert.type}
+          message={this.state.alert.message}
+        />
         <hr />
         <form onSubmit={this.handleSubmit}>
           <input
@@ -188,9 +211,6 @@ export default class EditMovie extends Component {
           <hr />
           <button className="btn btn-primary">Save</button>
         </form>
-        <div className="mt-3">
-          <pre>{JSON.stringify(this.state, null, 3)}</pre>
-        </div>
       </Fragment>
     );
   }
